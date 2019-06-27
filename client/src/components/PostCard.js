e2819332911be8cb08c83884f4ddd26b5263d7e7
Tobias Hjertelundh <tobias.hjertelundh@hotmail.com>
2019-06-27 14:42:09 +0200

    Display data in front with semantic ui

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+function PostCard({ post: { id, username, createdAt, body, likes } }) {
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <p>Buttons here</p>
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default PostCard;
