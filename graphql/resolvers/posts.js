31525fd4a90c0f3e5d9df747e1481cbcc73d974d
Tobias Hjertelundh <tobias.hjertelundh@hotmail.com>
2019-06-27 09:55:59 +0200

    crud complete posts comments and likes

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../utils/check-auth');
@@ -29,6 +29,11 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
+
+      if (args.body.trim() === '') {
+        throw new Error('Post must not be empty');
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
@@ -52,6 +57,22 @@ module.exports = {
       } catch {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find(like => like.username === username)) {
+          post.likes = post.likes.filter(like => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError('Post not found');
     }
   }
 };
