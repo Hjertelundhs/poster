3bde0aa777048073af8536977a557dd9a1a7b825
Tobias Hjertelundh <tobias.hjertelundh@hotmail.com>
2019-08-16 08:02:12 +0200

    parent caeabd491ac754cd0248ebb36a147d2b16bc44f5 author Tobias Hjertelundh <tobias.hjertelundh@hotmail.com> 1565935332 +0200 committer Tobias Hjertelundh <tobias.hjertelundh@hotmail.com> 1566375522 +0200
    change start scripts
    
    Fingers crossed
    
    xXx
    
    feg
    
    wdqf
    
    wqdwq
    
    Bump lodash from 4.17.11 to 4.17.15 in /client
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.11 to 4.17.15.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.11...4.17.15)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Remove Procfile
    
    dsdvs
    
    feddssa
    
    rgrge
    
    fwefw
    
    proc
    
    edfwe
    
    edqwd
    
    dsasd
    
    fussa
    
    wefwefwefwef
    
    efwefwe
    
    gefge
    
    efwedf
    
    ger
    
    wdfwedf
    
    ewfw
    
    wefw
    
    fewe
    
    ssss
    
    fdsfs
    
    wdwdw
    
    fww
    
    eeeehhh
    
    fewe
    
    dww
    
    efwef
    
    dw
    
    fweq
    
    dwdw
    
    efergwregw
    
    wqw
    
    dwdaw
    
    ewe
    
    ssaa
    
    fiqus
    
    drger
    
    cas
    
    ewfqqwe
    
    safsfas
    
    please me today
    
    mmuuuu
    
    efw
    
    cqecqwec
    
    jhjv
    
    sdcqscqa
    
    jhbvjhv
    
    aswdaqw
    
    jhjh
    
    wefw
    
    efqewf
    
    kkk
    
    lll
    
    fewefdwqedwe
    
    casda
    
    dsas
    
    bbjjj
    
    ed2ew2
    
    efwefw
    
    nhhjh
    
    ojkjb
    
    kjfkjfjk
    
    ddd
    
    cc
    
    xas
    
    hhhh
    
    dfwfd
    
    fdefd
    
    fwew
    
    ewfew
    
    dew
    
    efwe
    
    wwww

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-const { ApolloServer, PubSub } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server-express');
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
+//const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req, pubsub })
 });
 
+server.applyMiddleware({ app, path: '/graphql' });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('*', (req, res) => {
+  let urlen = res.sendFile(
+    path.resolve(__dirname, 'client', 'build', 'index.html')
+  );
+  console.log(MONGODB);
+});
+console.log('Hello');
+
+const port = process.env.PORT || 5000;
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI || MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
+    console.log({ port });
+    return app.listen({ port });
   })
   .then(res => {
-    console.log(`Server running at ${res.url}`);
+    console.log(`Server running at port ${port}`);
   });
